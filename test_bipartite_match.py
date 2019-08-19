661f15e87fbfa2e1019a55bda47e1240207142bf
Michael J. Curry <curry@cs.umd.edu>
2019-08-19 10:25:22 -0400

    slight change to make tests more interesting

--- test_bipartite_match.py
+++ test_bipartite_match.py
@@ -12,11 +12,11 @@ def test_ind_counts_to_longs():
 from bipartite_match import CurrentElems, toy_e_weights_type
 
 def unambiguous_matching():
-    currpool = CurrentElems([[torch.tensor(1), 0, 5], [torch.tensor(2), 0, 5], [torch.tensor(2), 0, 5]],
+    currpool = CurrentElems([[torch.tensor(2), 0, 5], [torch.tensor(1), 0, 5], [torch.tensor(2), 0, 5]],
                             [[torch.tensor(0), 0, 5]])
     e_weights = toy_e_weights_type()
-    correct_matching = torch.tensor([[1.0],
-                                     [0.0],
+    correct_matching = torch.tensor([[0.0],
+                                     [1.0],
                                      [0.0]])
     return currpool, e_weights, correct_matching
 
@@ -24,7 +24,7 @@ def unambiguous_matching():
 def test_get_matched_indices_1():
     currpool, e_weights, correct_matching = unambiguous_matching()
     lhs_inds, rhs_inds, _ = get_matched_indices(correct_matching, e_weights)
-    assert set(lhs_inds) == set([0])
+    assert set(lhs_inds) == set([1])
     assert set(rhs_inds) == set([0])
 
 def test_empty_match():
@@ -35,10 +35,10 @@ def test_empty_match():
 
 def test_screwy_match():
     currpool, e_weights, correct_matching = unambiguous_matching()
-    correct_matching[0,0] = 0.9
-    correct_matching[1,0] = 0.1
+    correct_matching[0,0] = 0.1
+    correct_matching[1,0] = 0.9
     lhs_inds, rhs_inds, _ = get_matched_indices(correct_matching, e_weights)
-    assert set(lhs_inds) == set([0])
+    assert set(lhs_inds) == set([1])
     assert set(rhs_inds) == set([0])
 
 
