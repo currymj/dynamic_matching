e53da22fa8bad3ef6699ab600f8a07607d21a29a
Michael J. Curry <curry@cs.umd.edu>
2019-08-19 10:39:08 -0400

    tests failing due to tiebreaking method

--- test_compute_matching.py
+++ test_compute_matching.py
@@ -16,3 +16,21 @@ def test_compute_matching_noweights():
 
     resulting_match, e_weights = compute_matching(currpool, torch.zeros(5), e_weights_type)
     assert torch.allclose(resulting_match, correct_matching)
+
+def ambiguous_matching():
+    currpool = CurrentElems([[torch.tensor(2), 0, 5], [torch.tensor(1), 0, 5], [torch.tensor(2), 0, 5]],
+                            [[torch.tensor(0), 0, 5], [torch.tensor(0), 0, 5]])
+
+
+    e_weights = toy_e_weights_type()
+    one_correct_matching = torch.tensor([[0.0, 0.0],
+                                     [1.0, 0.0],
+                                     [0.0, 0.0]])
+    return currpool, e_weights, one_correct_matching
+
+def test_compute_ambiguous():
+    # ties ought to be broken deterministically, by ordering (should jitter edge weights in this way)
+    currpool, e_weights_type, one_correct_matching = ambiguous_matching()
+
+    resulting_match, e_weights = compute_matching(currpool, torch.zeros(5), e_weights_type)
+    assert torch.allclose(resulting_match, one_correct_matching)
