302f99f19de1c26b352e0e5b9bc4eb8ecba581b9
Michael J. Curry <curry@cs.umd.edu>
2019-08-26 10:27:14 -0400

    some more test cases

--- test_opt.py
+++ test_opt.py
@@ -11,6 +11,32 @@ def test_compute_opt_basic():
 
     assert result_dict[0,0,0].x == 1.0
 
-def test_compute_opt_tiebreak():
-    pass
-    # make sure the tiebreaking works correctly (it should bc binary vars explicitly)
+def test_compute_opt_multi():
+    hist = History([[torch.tensor(1), 0, 2], [torch.tensor(1), 3, 4]], [[torch.tensor(1), 0, 2], [torch.tensor(1), 3, 4]])
+    max_t = 4
+    e_weights_type = toy_e_weights_type()
+
+    result_dict, model = opt_match(hist, max_t, e_weights_type)
+
+    assert result_dict[0,0,0].x == 1.0
+
+    assert result_dict[1,1,3].x == 1.0
+
+def test_compute_opt_smart():
+    hist = History([[torch.tensor(1), 0, 4], [torch.tensor(2), 3, 4]], [[torch.tensor(1), 0, 4], [torch.tensor(0), 3, 4]])
+    max_t = 4
+    e_weights_type = toy_e_weights_type()
+
+    result_dict, model = opt_match(hist, max_t, e_weights_type)
+
+    assert result_dict[0,0,0].x == 0.0
+
+    assert result_dict[1,1,3].x == 0.0
+
+    assert result_dict[0,1,3].x == 1.0
+    assert result_dict[1,0,3].x == 1.0
+
+
+# do we need tiebreaking or discounting? in this case we don't necessarily care what the optimal solution is, just its best score
+# determinism of course makes testing easier, but maybe that's not worth messing with the formulation of the problem
+
